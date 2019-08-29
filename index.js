2fb37cf11483cd3a96866a31b911bdd26c5f5ab8
Eric-Patrick-James <51387702+Eric-Patrick-James@users.noreply.github.com>
2019-08-29 14:11:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 3, 2019 23:00:00").getTime();
+var countDownDate = new Date("Aug 30, 2019 23:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -12,7 +12,7 @@ var x = setInterval(function() {
   now = now + timezone
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  var distance = now - countDownDate;
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
